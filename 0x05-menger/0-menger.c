fea3b579fd0bb8d4b4502b5926dffbdc7117416c
feliciahsieh <felicia@tixwriteoff.com>
2019-03-06 05:47:04 +0000

    a start. WIP

---
+++ 0x05-menger/0-menger.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * menger - implement 2D Menger sponge
+ * @level: level of 2D Menger sponge to create
+ * Return: N/A
+ */
+void menger(int level)
+{
+	if (level < 0)
+		return;
+
+}
