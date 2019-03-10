0160f11de1299fb172785005c6ff389509772cd5
feliciahsieh <felicia@tixwriteoff.com>
2019-03-10 05:19:07 +0000

    updated

--- 0x05-menger/0-menger.c
+++ 0x05-menger/0-menger.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+
+/**
+ * printMenger - print Menger 2D cube (Menger Face)
+ * @m: menger cube represented as an array
+ * @w: width of menger cube
+ * Return: N/A
+ */
+void printMenger(char *m, int w, int s)
+{
+	int i = 0;
+
+	printf("w: %d  s: %d\n", w, s);
+	for (i = 0; i < s; i++)
+	{
+		printf("%c", m[i]);
+		if (((i+1) % w) == 0)
+		  printf("\n");
+	}
+}
 
 /**
  * menger - implement 2D Menger sponge
@@ -8,7 +28,43 @@
  */
 void menger(int level)
 {
+	char *menger = NULL;
+	int size = 0, width = 0, i = 0, sizeHole = 0;
+
+
+	menger = menger;
+
 	if (level < 0)
 		return;
+	if (level == 0)
+	{
+		printf("#\n");
+		return;
+	}
 
+	width = pow(3, level);
+	size = width * width;
+
+	printf("level: %d\n", level);
+	printf("width: %d\n", width);
+	printf("size needed: %d\n", size + 1);
+
+	menger = malloc(size * sizeof(char) + 1);
+	if (menger == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		menger[i] = '#';
+	menger[i] = '\n';
+
+	/* poke holes */
+	sizeHole = pow(3, (level-1));
+	printf("sizeHole: %d\n", sizeHole);
+	for (i = 0; i < level; i++)
+	{
+		menger[size/2] = ' ';
+		printf("sizelocation: %d\n", size/2);
+	}
+
+	printMenger(menger, width, size);
 }
