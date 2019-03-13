ea1078fc11809a09b9d687cb6732fba68eff2ea7
feliciahsieh <felicia@tixwriteoff.com>
2019-03-13 16:43:32 +0000

    first commit

---
+++ 0x06-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "binary_trees.h"
+
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	unsigned int i = 0;
+
+	array = array;
+	size = size;
+	i = i;
+
+	if ((array == NULL) || (size == 0))
+		return (NULL);
+
+	print_array(array, size);
+
+	/* binary_tree_print(const binary_tree_t *tree); */
+
+	return (NULL);
+}
