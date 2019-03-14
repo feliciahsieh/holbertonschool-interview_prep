2f3f74d86f4af012507828de93b04bb40d4eb75d
feliciahsieh <felicia@tixwriteoff.com>
2019-03-14 19:00:02 +0000

    work in progress

--- 0x06-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x06-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -3,21 +3,77 @@
 
 #include "binary_trees.h"
 
+avl_t *leftRotate(avl_t *x)
+{
+	avl_t *y = x->right;
+	//avl_t *T2 = y->left;
+
+	/* Perform rotation */
+	y->left = x;
+	x->right = NULL;
+	x->parent = y;
+
+	/* Return new root */
+	return y;
+}
+
+avl_t *insert(avl_t *node, int n)
+{
+	avl_t *curr = NULL;
+
+	curr = curr;
+	if (node == NULL)
+	{
+	        node = malloc(sizeof(binary_tree_t));
+		node->left = NULL;
+		node->right = NULL;
+		node->n = n;
+		return (node);
+	} else
+	{
+		if (n < (node->n))
+		{
+			curr = NULL;
+			if (node->left == NULL)
+				curr = node;
+			node->left = insert(node->left, n);
+			if (curr != NULL)
+			{
+				node->left->parent = curr;
+				curr = NULL;
+			}
+		}
+		else
+		{
+			curr = NULL;
+			if (node->right == NULL)
+				curr = node;
+			node->right = insert(node->right, n);
+			if (curr != NULL)
+			{
+				node->right->parent = curr;
+				curr = NULL;
+			}
+		}
+	}
+
+	return(node);
+}
 
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
+	avl_t *avl = NULL;
+	avl_t *curr = NULL;
 	unsigned int i = 0;
 
-	array = array;
-	size = size;
-	i = i;
-
+	curr = curr;
 	if ((array == NULL) || (size == 0))
 		return (NULL);
 
-	print_array(array, size);
+	for (i = 0; i < size; i++)
+	{
+		avl = insert(avl, array[i]);
+	}
 
-	/* binary_tree_print(const binary_tree_t *tree); */
-
-	return (NULL);
+	return (avl);
 }
--- 0x06-sorted_array_to_avl/README.md
+++ 0x06-sorted_array_to_avl/README.md
@@ -1,4 +1,7 @@
-# 0x06 - Array to AVL
+# 0x06 - Array to AVL Tree (Adelson-Velsky and Landis)
+
+An AVL tree is a self-balancing tree.
+https://en.wikipedia.org/wiki/AVL_tree
 
 ```
 gcc -Wall -Wextra -Werror -pedantic binary_tree_print.c 0-main.c 0-sorted_array_to_avl.c-o 0-sorted_array_to_avl
