87b8774325917e3c2326c20c08580b3eec38a3ae
feliciahsieh <felicia@tixwriteoff.com>
2019-03-19 22:08:46 +0000

    first commit

---
+++ 0x07-sandpiles/0-sandpiles.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - implement sandpiles - fractal x + a = a
+ * @grid1: first grid to add
+ * @grid2: second grid to add
+ * Return: N/A
+ */
+void sandpiles_sum(int grid1[][], int grid2[][])
+{
+
+}
---
+++ 0x07-sandpiles/README.md
@@ -0,0 +1,8 @@
+# 0x07. Sandpiles
+
+Fractal-based Identity matrix
+https://en.wikipedia.org/wiki/Abelian_sandpile_model
+
+```
+gcc -Wall -Wextra -Werror -pedantic 0-main.c 0-sandpiles.c -o 0-sandpiles
+```
