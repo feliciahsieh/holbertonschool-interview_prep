6904ad706d75049e3d88897393079cbfe6f6fdf7
feliciahsieh <felicia@tixwriteoff.com>
2019-03-20 00:59:22 +0000

    latest

--- 0x06-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x06-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -6,21 +6,40 @@
 avl_t *leftRotate(avl_t *x)
 {
 	avl_t *y = x->right;
-	//avl_t *T2 = y->left;
+	avl_t *T2 = y->left;
 
 	/* Perform rotation */
 	y->left = x;
-	x->right = NULL;
+	x->right = T2;
 	x->parent = y;
 
 	/* Return new root */
 	return y;
 }
 
+avl_t *quick(avl_t *avl, int array[], size_t n, size_t low, size_t high)
+{
+	size_t mid = (low + high) / 2;
+
+	low = low;
+	high = high;
+	mid = mid;
+	n = n;
+
+	avl = insert(avl, array[mid], low, high);
+	avl = quick(avl, array, n, low, mid - 1);
+	avl = insert(avl, array[mid], low, high);
+	avl = quick(avl, array, n, mid + 1, high);
+
+	return (avl);
+}
+
 avl_t *insert(avl_t *node, int n)
 {
 	avl_t *curr = NULL;
+	int mid = 0;
 
+	mid = mid;
 	curr = curr;
 	if (node == NULL)
 	{
@@ -60,20 +79,35 @@ avl_t *insert(avl_t *node, int n)
 	return(node);
 }
 
-avl_t *sorted_array_to_avl(int *array, size_t size)
+avl_t *sorted_array_to_avl(int array[], size_t size)
 {
 	avl_t *avl = NULL;
 	avl_t *curr = NULL;
-	unsigned int i = 0;
+	size_t i = 0;
+	size_t mid = 0;
 
+	i = i;
+	mid = mid;
 	curr = curr;
 	if ((array == NULL) || (size == 0))
 		return (NULL);
 
+	avl = quick(avl, array, 0, size-1);
+
+	/*
 	for (i = 0; i < size; i++)
 	{
-		avl = insert(avl, array[i]);
+		printf("Inserting #i: %d\n", i);
+		index = getIndex(0, size);
+		avl = insert(avl, array[index]);
 	}
+	*/
+
+	/*curr = avl;
+	for (i = 0; i < size-4; i++)
+		;
+	avl = leftRotate(curr);
+	*/
 
 	return (avl);
 }
