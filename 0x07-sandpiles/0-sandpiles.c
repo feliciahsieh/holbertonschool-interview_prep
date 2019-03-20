6114a1afa0009aca70038d3a2fda622727e9a581
feliciahsieh <felicia@tixwriteoff.com>
2019-03-20 00:45:28 +0000

    working version

--- 0x07-sandpiles/0-sandpiles.c
+++ 0x07-sandpiles/0-sandpiles.c
@@ -4,12 +4,167 @@
 #include "sandpiles.h"
 
 /**
+ * printGrid - print gridsum
+ * @grid: first grid to add
+ * Return: N/A
+ */
+static void printGrid(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+/**
+ * isStableGrid - check if all cells are 0, 1, 2, or 3 only
+ * @grid: grid to check
+ * Return: 1 if TRUE, 0 if FALSE
+ */
+int isStableGrid(int grid[3][3])
+{
+	int i = 0, j = 0;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if ((grid[i][j] < 0) || (grid[i][j] > 3))
+				return (0);
+	return (1);
+}
+
+/**
  * sandpiles_sum - implement sandpiles - fractal x + a = a
  * @grid1: first grid to add
  * @grid2: second grid to add
  * Return: N/A
  */
-void sandpiles_sum(int grid1[][], int grid2[][])
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
+	int i = 0, j = 0;
+	int grid3[3][3] = {0};
 
+	grid3[0][0] = grid3[0][0];
+
+	if ((grid1 == NULL) || (grid2 == NULL))
+		return;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid3[i][j] = grid1[i][j] + grid2[i][j];
+
+	while (!isStableGrid(grid3))
+	{
+		printf("=\n");
+		printGrid(grid3);
+
+		/* Copy grid3 to grid1 and clear grid3 */
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				grid1[i][j] = grid3[i][j];
+				grid3[i][j] = 0;
+			}
+		}
+
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				/* Topple */
+				if (grid1[i][j] > 3)
+					grid3[i][j] = grid1[i][j] - 4;
+				else
+					grid3[i][j] = grid1[i][j];
+
+				if ((i == 0) && (j == 0))
+				{
+					if (grid1[1][0] > 3)
+						grid3[i][j]++;
+					if (grid1[0][1] > 3)
+						grid3[i][j]++;
+				}
+				else if ((i == 0) && (j == 1))
+				{
+					if (grid1[0][0] > 3)
+						grid3[i][j]++;
+					if (grid1[1][1] > 3)
+						grid3[i][j]++;
+					if (grid1[0][2] > 3)
+						grid3[i][j]++;
+				}
+				else if ((i == 0) && (j == 2))
+				{
+					if (grid1[0][1] > 3)
+						grid3[i][j]++;
+					if (grid1[1][2] > 3)
+						grid3[i][j]++;
+				}
+				else if ((i == 1) && (j == 0))
+				{
+					if (grid1[0][0] > 3)
+						grid3[i][j]++;
+					if (grid1[2][0] > 3)
+						grid3[i][j]++;
+					if (grid1[1][1] > 3)
+						grid3[i][j]++;
+				}
+				else if ((i == 1) && (j == 1))
+				{
+					if (grid1[1][0] > 3)
+						grid3[i][j]++;
+					if (grid1[0][1] > 3)
+						grid3[i][j]++;
+					if (grid1[2][1] > 3)
+						grid3[i][j]++;
+					if (grid1[1][2] > 3)
+						grid3[i][j]++;
+				}
+				else if ((i == 1) && (j == 2))
+				{
+					if (grid1[1][1] > 3)
+						grid3[i][j]++;
+					if (grid1[0][2] > 3)
+						grid3[i][j]++;
+					if (grid1[2][2] > 3)
+						grid3[i][j]++;
+				}
+				else if ((i == 2) && (j == 0))
+				{
+					if (grid1[1][0] > 3)
+						grid3[i][j]++;
+					if (grid1[2][1] > 3)
+						grid3[i][j]++;
+				}
+				else if ((i == 2) && (j == 1))
+				{
+					if (grid1[2][0] > 3)
+						grid3[i][j]++;
+					if (grid1[1][1] > 3)
+						grid3[i][j]++;
+					if (grid1[2][2] > 3)
+						grid3[i][j]++;
+				}
+				else if ((i == 2) && (j == 2))
+				{
+					if (grid1[2][1] > 3)
+						grid3[i][j]++;
+					if (grid1[1][2] > 3)
+						grid3[i][j]++;
+				}
+			}
+		}
+	}
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid3[i][j];
 }
--- 0x07-sandpiles/sandpiles.h
+++ 0x07-sandpiles/sandpiles.h
@@ -1,7 +1,6 @@
 #ifndef SANDPILE_H
 #define SANDPILE_H
 
-/* sandpile.h */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
 #endif
