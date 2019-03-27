9de3ff0d77bf70bcfbc003f5778f8e44ffd9551c
feliciahsieh <felicia@tixwriteoff.com>
2019-03-27 02:41:04 +0000

    first commit

---
+++ 0x08-linear_skip/0-linear_skip.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+#include "search.h"
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	list = list;
+	value = value;
+
+	return (NULL);
+}
---
+++ 0x08-linear_skip/README.md
@@ -0,0 +1,5 @@
+# 0x08. Linear search in skip list
+
+```
+gcc -Wall -Wextra -Werror -pedantic 0-main.c 0-linear_skip.c skiplist/*.c -lm -o 0-linear_skip
+```
