7c28730ab05a8d6b4f13d2562940d368ade40325
feliciahsieh <felicia@tixwriteoff.com>
2019-04-01 14:08:49 +0000

    account for duplicate values so that first occurence is returned

--- 0x08-linear_skip/0-linear_skip.c
+++ 0x08-linear_skip/0-linear_skip.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 #include "search.h"
 
@@ -12,8 +11,9 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	int count = 0, listSize = 0, skipSize = 0, i = 0;
-	skiplist_t *curr = list, *prev = list;
+	int listSize = 0;
+	size_t i = 0;
+	skiplist_t *curr = list, *prev = NULL;
 
 	if ((list == NULL) || (value < (list->n)))
 		return (NULL);
@@ -21,34 +21,28 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		return (list);
 	for (curr = list; curr != NULL; curr = curr->next)
 		listSize++;
-	skipSize = sqrt(listSize);
-	curr = list->express;
-	count += skipSize;
+	curr = list;
 	while ((curr != NULL) && ((curr->n) < value) && (curr->express != NULL))
 	{
-		printf("Value checked at index [%d] = [%d]\n", count, curr->n);
-		count += skipSize;
 		prev = curr;
 		curr = curr->express;
+		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
 	}
-	printf("Value checked at index [%d] = [%d]\n", count, curr->n);
-	curr = prev;
-	count -= skipSize;
-	if (curr->express->n <= value)
+	if (curr->n <= value)
+		curr = prev;
+	if (curr->express == NULL)
 	{
+		curr = NULL;
 		i = listSize - 1;
-		curr = curr->express;
-		count += skipSize;
 	}
 	else
-		i = count + skipSize;
-	printf("Value found between indexes [%d] and [%d]\n", count, i);
-	for (; (curr != NULL) && (curr->n <= value); count++)
+		i = curr->express->index;
+	printf("Value found between indexes [%lu] and [%lu]\n", curr->index, i);
+	for (; (curr != NULL) && (curr->n <= value); curr = curr->next)
 	{
-		printf("Value checked at index [%d] = [%d]\n", count, curr->n);
+		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
 		if (curr->n == value)
 			return (curr);
-		curr = curr->next;
 	}
 	return (NULL);
 }
