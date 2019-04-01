d2fde627183999b307178710d766608907782cbe
feliciahsieh <felicia@tixwriteoff.com>
2019-04-01 15:17:11 +0000

    first commit with code

--- 0x09-heap_sort/sort.h
+++ 0x09-heap_sort/sort.h
@@ -1,6 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
+void siftDown(int *heap, size_t root, size_t end, size_t size);
+void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
 
 #endif /* SORT_H */
